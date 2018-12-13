d35d37b11a22fd2545c456e283b4c539c98b3aac
Andreas Putz <putz.andi@gmail.com>
2018-12-13 13:10:45 -0800

    [major] - Added Training

--- model.py
+++ model.py
@@ -27,14 +27,35 @@ import argparse
 import os
 
 import scipy as sp
+import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
 
 import sklearn
 from sklearn.model_selection import train_test_split
-#import tensorflow as tf
-#import keras
 
+# Tensorflow import
+import tensorflow as tf
+#import tensorflow.keras as keras
+from tensorflow.python.client import device_lib
+
+#############################################
+# Keras Import
+#############################################
+# Keras model definition
+import keras
+from keras.models import Sequential
+from keras.layers import Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Reshape
+from keras.layers.convolutional import Convolution2D, Conv2D
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+# Keras training
+from keras.models import Model, load_model
+from keras.optimizers import Adagrad
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 import logging
 
@@ -54,27 +75,27 @@ class DriveData(object):
     Class to import the steering file as well as the camera images and
     present them memory efficiently.
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
+      attr1 (str): Description of `attr1`.
+      attr2 (:obj:`int`, optional): Description of `attr2`.
     """
 
-    def __init__(self, path='./data'):
+    def __init__(self, path='./data', batchsize=1024):
         """Initializer of the DriveData object
 
         Args:
             path (str): Path to the data directory `param1`.
         """
 
+        self._batchsize = batchsize
+
         path = os.path.expanduser(path)
-        self._load_data(path=path)
-
-        # Create attributes:
         self._path = path
-        self.generator_train = self.generator(samples=self._samples_train,
-            batch_size=32,path=path
-        )
 
-    def _load_data(self, path='./data'):
+        # Load data and create generators
+        self._load_data()
+
+
+    def _load_data(self):
         """Load the driving logfile and split in training vs validation set.
 
         TODO:
@@ -86,6 +107,7 @@ class DriveData(object):
         """
 
         logger.debug('DriveData._load_csv: Load csv file')
+        path = self._path
 
         # Read data
         df = pd.read_csv(os.path.join(path,'driving_log.csv'))
@@ -100,15 +122,13 @@ class DriveData(object):
         self._samples_train = train_samples
         self._samples_validation = validation_samples
 
-        self.generator_train = self.generator(samples=self._samples_train,
-            batch_size=32,path=path
-        )
+        self.generator_train = self.generator(samples=self._samples_train)
+        self.generator_validation = self.generator(samples=self._samples_validation)
 
-        self.generator_validation = self.generator(samples=self._samples_validation,
-            batch_size=32,path=path
-        )
+    def get_size(self):
+        return len(self._samples_train), len(self._samples_validation)
 
-    def generator(self,samples=None, batch_size=32,path='./data'):
+    def generator(self,samples=None):
         """Generator to only hold batch_size of data in memory
 
         TODO:
@@ -123,6 +143,8 @@ class DriveData(object):
         print(" Generate batch data")
         print("-"*50)
 
+        path = self._path
+        batchsize = self._batchsize
         num_samples = len(samples)
         print(f" Batch size = {batch_size}, Sample Size = {num_samples}")
 
@@ -150,17 +172,144 @@ class DriveData(object):
 
 
 class KerasCNN(object):
-    """Preprocessing and training of Keras cnn models for autonomous driving.
+  """Preprocessing and training of Keras cnn models for autonomous driving.
 
-    Class to import the steering file as well as the camera images and
-    present them memory efficiently.
-    Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
+  Class to import the steering file as well as the camera images and
+  present them memory efficiently.
+  Attributes:
+    attr1 (str): Description of `attr1`.
+    attr2 (:obj:`int`, optional): Description of `attr2`.
+  """
+
+  def __init__(self,cnn_type='basic', tf_device_type='CPU', input_shape=(160,320,3),model='nvidia'):
+
+    logger.info('Initialising KerasCNN Class')
+
+    self._input_shape = input_shape
+    # Initialize Keras
+    tf_device = self.get_device_list(tf_device_type=tf_device_type)
+    self.keras_initialize(tf_device=tf_device)
+
+    if model == 'nvidia':
+      logger.info('KerasCNN.init: Use nvidia model')
+      self.model_input()
+      self.model_nvidia_original()
+
+
+  def get_device_list(self, tf_device_type=None):
+    """List all available compute devices (GPU, CPU)
+
+    Args:
+      device_type (string, optional): Filter for computer devices.
+        Should be either "CPU" or "
     """
+    local_device_protos = device_lib.list_local_devices()
+    if tf_device_type:
+      return [x.name for x in local_device_protos if x.device_type == tf_device_type]
+    else:
+      return [x.name for x in local_device_protos]
 
-    def __init__(self):
-        print("Hallo")
+  def keras_initialize(self, tf_device=None):
+
+    self._keras_device = tf_device
+
+    with tf.device('/gpu:0'):
+      self._model = Sequential()
+
+  def model_input(self, cropping=((40,25), (0,0)), resizing=True, new_size=(66,200)):
+    """Create input layers for model
+
+    Args:
+      cropping (list, optional): Pixel to be cropped:
+        ((y_top, y_bottom), (x_left, e_right))
+      resizing (bool, optional): Resize to nvidia model expected input.
+        default: True
+      new_size (list, optional): New image size. Only active is resizing=True.
+        default: (66,200)
+
+
+    """
+    with tf.device('/gpu:0'):
+      self._model.add(Cropping2D(cropping=cropping, input_shape=self._input_shape))
+      if resizing:
+        self._model.add(Lambda(lambda x: tf.image.resize_images(x, new_size))) # resize image
+      self._model.add(Lambda(lambda x: x/255.0 -0.5)) # normalization
+
+  def model_nvidia_original(self, init = 'glorot_normal', activation = 'relu', dropout=(0.5,0.7),
+                                  layers_dense=(100,50,10,1)):
+    """ NVIDIA CNN for self driving (Modified to reduce DOF)
+
+    Args:
+      init (string, optional): Method to initialize weights.
+        Default: 'glorot_normal'
+      activation (string, optional): Activation function for CNN
+        Default: 'relu'
+      dropout (list, optional): Dropout rate for conv and dense layers.
+        Defaut: ('0.5','0.7'), 1st element: Conv, 2nd element: dense
+      layers_dense (list, optional): Size of the dense layers. Constructs as many dense
+        layers as specified in the list. Default: (100,50,10,1)
+    """
+    with tf.device('/gpu:0'):
+      # Convnet
+      self._model.add(Conv2D(24,(5,5), strides=(2,2), padding='valid', kernel_initializer=init,activation=activation))
+      self._model.add(Dropout(dropout[0]))
+
+      self._model.add(Conv2D(36,(5,5), strides=(2,2), padding='valid', kernel_initializer=init,activation=activation))
+      self._model.add(Dropout(dropout[0]))
+
+      self._model.add(Conv2D(48,(5,5), strides=(2,2), padding='valid', kernel_initializer=init,activation=activation))
+      self._model.add(Dropout(dropout[0]))
+
+      self._model.add(Conv2D(64,(3,3), kernel_initializer=init,activation=activation))
+      self._model.add(Dropout(dropout[0]))
+
+      self._model.add(Conv2D(64,(3,3), kernel_initializer=init,activation=activation))
+      self._model.add(Dropout(dropout[0]))
+
+      # Dense Layers
+      self._model.add(Flatten())
+      for dense_size in layers_dense:
+        self._model.add(Dense(dense_size, kernel_initializer=init))
+        if (dense_size !=1):
+          self._model.add(Dropout(dropout[1]))
+
+
+  def run_training(self, train_data=None, train_size=None,
+                   val_data=None, val_size=None,
+                   batch_size=1024, nb_epoch=100):
+
+    if not dataset:
+      logger.error('No dataset provided')
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
+
+    with tf.device('/gpu:0'):
+      self._model.compile(loss='mse', optimizer='Adagrad')
+
+      model.fit_generator(train_data, steps_per_epoch=train_size,
+            validation_data=val_data, validation_steps=val_size,
+            nb_epoch=nb_epoch)
+
+
+    print(history_object.history.keys())
+    # Plot the training and validation loss for each epoch
+    history_ = history_object.history
+    print(history_.keys())
+    sp.save('history_obj_origin.npy', history_)
+
+    plt.figure()
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('Mean Squared Error of model ...')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.ylim([0, 0.1])
+    plt.show()
+
+
+
+
 
 
 def main():
@@ -179,8 +328,11 @@ def main():
     print(args.path)
 
     # Load the data:
-
     mydata = DriveData(path=args.path)
+    mycnn  = KerasCNN()
+
+    # Create model class
+
 
 
 ########################################################################################
